236cb722556f2ba927e939c2b9e95f112a3671f3
betziii <se.bethelhem.teshibelay@gmail.com>
2022-04-01 16:05:54 +0300

    Best Time to Buy and Sell Stock with Cooldown

---
+++ bootcamp/Dynamic Programming/best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dicts = {}
+#         bs : buy sell boolean if true buy if false sell
+        def dfs(i, bs):
+            if i >= len(prices):
+                return 0
+            if (i,bs) in dicts:
+                return dicts[(i,bs)]
+            cooldown = dfs(i + 1, bs)
+            if bs:
+                buy = dfs(i + 1, not bs) - prices[i]
+                dicts[(i, bs)] = max(buy, cooldown)
+            else:
+                sell = dfs(i + 2, not bs) + prices[i]
+                dicts[(i, bs)] = max(sell, cooldown)
+            return dicts[(i, bs)]
+        return dfs(0, True)
