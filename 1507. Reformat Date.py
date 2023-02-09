e49fad9dc44de240a2826d1120bc7d75f4d70d91
betziii <56472671+betziii@users.noreply.github.com>
2023-02-09 20:58:46 +0300

    Create 1507. Reformat Date.py

---
+++ 1507. Reformat Date.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reformatDate(self, date: str) -> str:
+        months = {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06", "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"}
+        
+        year = date.split()[2]
+        month = date.split()[1]
+        day = date.split()[0]
+        formatd = ""
+        
+        for c in day:
+            if not c.isdigit():
+                break
+            else:
+                formatd += c 
+                
+        if len(formatd) == 1:
+            formatd = "0" + formatd
+        
+        reformat = year + "-" + months[month] + "-" + formatd
+        
+        
+        return reformat
